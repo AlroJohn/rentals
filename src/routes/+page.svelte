89855e59d1a51a4115fccd36caaf1917e609a66e
Alro John <alromercado08@gmail.com>
2025-03-23 23:02:44 +0800

    transition to sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	// import ThemeButton from '../custom/ThemeButton.svelte';
+</script>
+
+<section class="container mx-auto justify-center items-center min-h-screen flex flex-col gap-2">
+	<h1 class="text-3xl font-bold">Page</h1>
+	<p>This is a page.</p>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+	<Button>Click me!</Button>
+	<!-- <ThemeButton /> -->
+</section>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
