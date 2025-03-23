def203b6aa6961f90587336d865411b2355b7457
Alro John <alromercado08@gmail.com>
2025-03-23 23:32:32 +0800

    done with the routing testing

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import ThemeButton from '../custom/ThemeButton.svelte';
+	import ThemeButton from '../../custom/ThemeButton.svelte';
 </script>
 
 <section class="container mx-auto justify-center items-center min-h-screen flex flex-col gap-2">
-	<h1 class="text-3xl font-bold">Page</h1>
-	<p>This is a page.</p>
-	<h1 class="text-3xl font-bold underline">Hello world!</h1>
-	<a href="/about">
+	<h1 class="text-3xl font-bold">About Page</h1>
+	<p>This is a about page.</p>
+	<a href="/">
 		<Button>Click me!</Button>
 	</a>
 	<ThemeButton />
 </section>
 
 <svelte:head>
-	<title>Homepage</title>
+	<title>About</title>
 </svelte:head>
