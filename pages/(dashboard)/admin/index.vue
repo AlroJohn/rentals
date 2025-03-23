bf12a5651e24d2a83585972ebb484feea8a3739c
Alro John <alromercado08@gmail.com>
2025-03-23 22:38:53 +0800

    transition to sveltekit

--- pages/(dashboard)/admin/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="flex justify-center items-center h-screen">
-    <NuxtLink to="/"><Button>Click to Home</Button></NuxtLink>
-  </div>
-</template>
